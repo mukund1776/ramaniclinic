da9520d93c01260c049d222b1567f82e25f0262a
mukund1776 <mukund1776@gmail.com>
2025-02-05 17:04:11 +0530

    Add vaishnavi clinic logo

--- RamaniDentalClinic/src/app/header/header.component.ts
+++ RamaniDentalClinic/src/app/header/header.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from "./navbar/navbar.component";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'ac-header',
   standalone: true,
-  imports: [NavbarComponent],
+  imports: [CommonModule, NavbarComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
-
+  isAnandClinic = window.location.href.includes('vaishnavismilenest');
 }
